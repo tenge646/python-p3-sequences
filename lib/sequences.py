83679cf3610d796894c237cccb994a0e9df77a66
tenge646 <144627741+tenge646@users.noreply.github.com>
2023-11-24 16:34:44 +0300

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print('[]')
+        return
+    elif length == 1:
+        print('[0]')
+        return
+
+    fibonacci_sequence = [0, 1]
+    for i in range(2, length):
+        fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+
+    print(fibonacci_sequence)
